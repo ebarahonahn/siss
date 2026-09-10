2467d3d7a69f192789c44bccb631e3ee4e23d623
Edgar Ernesto Barahona Flores <edgar0371@gmail.com>
2026-09-10 10:08:20 -0600

    Actualización de rutas de frontend

--- siss-frontend/src/environments/environment.ts
+++ siss-frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api/v1',
-  socketUrl: 'http://localhost:3000',
+  apiUrl: 'https://siss-backend.onrender.com/api/v1',
+  socketUrl: 'https://siss-backend.onrender.com',
 };
